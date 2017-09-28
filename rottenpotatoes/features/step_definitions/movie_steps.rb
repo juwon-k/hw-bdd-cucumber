591bc0c713346f02069d1f32562e1726d085b40c
Juwon Kim <juwon@berkeley.edu>
2017-09-28 20:54:37 +0000

    work in progress, did part 3 and on part 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+  Movie.create movie
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,21 +18,31 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  assert page.body =~ /#{e1}.*{e2}/m, "#{e1} was not before #{e2}"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
+# "When I check the following ratings: G"
+# "When I uncheck the following ratings: PG, G, R"
 
+When (/^I check the following ratigs: G,PG,PG\-(\d+),R$/) do |rating|
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  # "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |r|
+    if uncheck
+      uncheck "ratings_#{r}"
+    else
+      check "ratings_#{r}"
+    end
+  end
 end
 
-Then /I should see all the movies/ do
+Then (/^I should see all of the movies$/) do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  all_movies = Movie.all
+  all_movies.each do |movie|
+    assert page.body =~ /#{movie.title}/m, "#{movie.title} was missing from page"
+  end
 end
